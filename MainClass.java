2d1a3de6193edf99853520401981c6d1827467bd
Fernando <freinando0@gmail.com>
2017-08-08 01:10:48 +0000

    a

--- MainClass.java
+++ MainClass.java
@@ -2,7 +2,13 @@ import java.io.*;
 public class MainClass{
     public static void main(String args[]){
         try{
-            MyLexer lexer   = new MyLexer(new FileInputStream(new File("input.prog")));
+            String arquivo = "input.prog";
+            
+            if (args.length != 0){
+                arquivo = args[0];
+            }
+            
+            MyLexer lexer   = new MyLexer(new FileInputStream(new File(arquivo)));
             MyParser parser = new MyParser(lexer);
             parser.init();
             parser.prog();
---
+++ exec.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+if [ -z "$1" ]; then
+    echo "Executando MainClass sem parâmetro!"
+    java -cp ./bin:antlr.jar:. MainClass
+else
+    echo "Executando MainClass com o parâmetro: $1"
+    java -cp ./bin:antlr.jar:. MainClass $1
+fi
